22a8747d494e86cf43cf4b5c4dd26189ecc2b425
Thilo Will <thilow@microsoft.com>
2017-02-15 09:49:16 +0100

    word_rnn_test: get files for data

--- Tests/EndToEndTests/CNTKv2Python/Examples/word_rnn_test.py
+++ Tests/EndToEndTests/CNTKv2Python/Examples/word_rnn_test.py
@@ -20,9 +20,7 @@ def test_word_rnn(device_id):
     # Setting global parameters
     W.use_sampled_softmax = True
     W.softmax_sample_size = 3
-
     W.use_sparse = True
-
     W.hidden_dim = 20
     W.num_layers = 2
     W.num_epochs = 1
@@ -32,11 +30,14 @@ def test_word_rnn(device_id):
     W.learning_rate = 0.02
     W.momentum_as_time_constant = 5
     W.clipping_threshold_per_sample = 5.0
-    W.token_to_id_path        = 'test/token2id.txt'
-    W.validation_file_path    = 'test/text.txt'
-    W.train_file_path         = 'test/text.txt'
-    W.token_frequencies_file_path = 'test/freq.txt'
     W.segment_sepparator = '<eos>'
     W.num_samples_between_progress_report = 2
+
+    # Get path to data files
+    dir =  os.path.dirname( os.path.abspath(W.__file__))
+    W.token_to_id_path            = os.path.join(dir, 'test\\token2id.txt')
+    W.validation_file_path        = os.path.join(dir, 'test\\text.txt')
+    W.train_file_path             = os.path.join(dir, 'test\\text.txt')
+    W.token_frequencies_file_path = os.path.join(dir, 'test\\freq.txt')
 
     W.train_lm()
